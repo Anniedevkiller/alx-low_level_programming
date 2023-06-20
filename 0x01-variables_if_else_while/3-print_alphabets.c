f2f3e23ead09e17b958de78f9f40bd7427db28b2
Tiffany <erisannieannie@gmail.com>
2023-06-19 20:20:43 -0700

    programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main .Prints the alphabet
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghikjklmmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		int i;
+
+	for (i = 0: i < 52; i**)
+	{
+		putchar('\n');
+		return (0);
+	}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48: i < 103; 1++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

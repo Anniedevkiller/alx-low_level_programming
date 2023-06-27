bd7ae760935ee56a76e5e80a982efc39ffcd7e66
Tiffany <erisannieannie@gmail.com>
2023-06-26 18:15:59 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns teh length of a string
+ * @s : strings
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return(length);
+}

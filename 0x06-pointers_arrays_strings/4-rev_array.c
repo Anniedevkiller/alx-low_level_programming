68479603aab6b9e3db59e105438bd1b9b83fb2a6
Tiffany <erisannieannie@gmail.com>
2023-06-29 20:57:39 -0700

    loops

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main entry 
+ * reverse the content of an array of interger
+ * Return on success
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}

ef6e010dfa8dfdf1a9816a5d548f52987e7e7b84
Tiffany <erisannieannie@gmail.com>
2023-07-01 11:02:49 -0700

    loop

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,23 +8,32 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
+	int i;
+	int j;
 
 	if ( n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i l= 98)
-				printf("%d, ", i);
-		}
-	} else if (n >= 98)
-	{
-		for (j = n; j > = 98; j--)
-		{
-			if (j != 98 )
-				printf("%d, ', j);
-			else id (j == 98)
-				printf("%d\n", j);
+			if (i != 98)
+				{
+					printf("%d ", i);
+				}
+	 else if (i == 98)
+	 {
+		 printf("%d\n", i);
+	 }
 		}
 	}
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+			{
+				printf("%d, ", j);
+			}
+			else if (j == 98)
+			{
+				printf("%d\n", j);
+			}
+		}
 }

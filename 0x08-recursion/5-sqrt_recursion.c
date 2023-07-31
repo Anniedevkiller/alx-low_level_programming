df2d094bd0b020e55ff9bdbeef08da76d7838f28
Tiffany <erisannieannie@gmail.com>
2023-07-31 04:18:25 -0700

    r

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,31 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * function : that return the natural square root of a numbeer[
  */
+int sqrtfinder(int on, int off, int n)
+{
+	long a;
+
+	if (off >= on)
+	{
+		a = on +(off - on)
+			/ 2;
+		if (a * a == n)
+			return (a);
+		if (a * a > n)
+			return (sqrtfinder(on, a - 1, n));
+		if (a * a < n)
+			return (sqrtfinder(a + 1, off, n));
+	}
+	return(-1);
+}
+
 int _sqrt_recursion(int n)
 {
-        if (n % 2 != 0)
-	{
-	return (-1);
-	}
-    else if (n > 1)
-    {
-        return (n / 2 * (_sqrt_recursion(n / 2)));
-    }
-  else if (n == 1)
-  {
-  return (1);
-  }
-}  
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqrtfinder(2, n, n));
+}

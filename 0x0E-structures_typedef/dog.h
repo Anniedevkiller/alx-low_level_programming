73dc3c5d05ff7b8499da90ad7e860478de071517
Tiffany <erisannieannie@gmail.com>
2023-08-01 07:43:31 -0700

    m

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,6 +7,8 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+char *_strcpy(char *dest, char *src);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

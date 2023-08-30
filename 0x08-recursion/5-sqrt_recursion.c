e4e66b2cd4c2a6a48e75623a0f65c77c9a775029
Anniedevkiller <erisannieannie@gmail.com>
2023-08-29 17:10:40 -0700

    recur

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,43 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
-* _sqrt_recursion - Returns the natural square root of a number
-*
-* @n: The number to calculate the square root of
-*
-* Return: The natural square root of n, or -1 if n is negative
-*/
+ * sqrtfinder - finds square root
+ *
+ * @on:start of input
+ * @off: end of input
+ * @n: passed input
+ * Return: returns int
+ */
+
+int sqrtfinder(int on, int off, int n)
+{
+	long a;
+
+	if (off >= on)
+	{
+		a = on + (off - on) / 2;
+		if (a * a == n)
+			return (a);
+		if (a * a >  n)
+			return (sqrtfinder(on, a - 1, n));
+		if (a * a < n)
+			return (sqrtfinder(a + 1, off, n));
+	}
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - finds squareroot
+ *
+ * @n: int input
+ * Return: int value
+ */
+
 int _sqrt_recursion(int n)
 {
-	if (n % 2 != 0)
-	{
+	if (n < 0)
 		return (-1);
-	}
-	else if (n > 1)
-	{
-		return (n / 2 * (_sqrt_recursion(n / 2)));
-	}
-
-	else if (n == 1)
-	{
-		return (1);
-	}
-
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqrtfinder(2, n, n));
 }

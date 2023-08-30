8a6582000de0cc5cae52da92a5eda3aa58289cab
Anniedevkiller <erisannieannie@gmail.com>
2023-08-29 17:44:20 -0700

    argc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main -  prints each word in a sentence to a new line
+ * @argc: argument count
+ * @argv: argument velocity
+ *
+ * Return: Always 0, Success
+ *
+ * Author: Destiny Saturday (DestinedCodes)
+ * Date: 12/06/2023
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
